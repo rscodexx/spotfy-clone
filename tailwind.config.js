8e9e84affeca7f7240bdca97dc99b084bdba2f18
Rafael Santos <raffrenan@gmail.com>
2022-05-22 16:07:58 -0300

    Add extend configs.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,30 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      '14': '3.5rem',
+      '125px': '125px',
+    },
+    colors: {
+      'hoverspt': '#18D760',
+      'purple-main': '#2D46B9',
+      'green-main': '#1ED760',
+    },
+    backgroundImage: theme => ({
+      'spotfy-theme': "url('/assets/img/burts.svg')",
+      'spotfy-theme-mobile': "url('/assets/img/burts-mobile.svg')"
+    }),
+    backgroundSize: {
+      '175%': '175%',
+      '195%': '195%',
+    },
+    backgroundPosition: {
+      'banner': '46% 4%',
+      'banner-mobile': 'top 25% center',
+    },
+    fontSize: {
+      '9xl': '9rem',
+    }
   },
   plugins: [],
 }
