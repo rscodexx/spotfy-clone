fe9cbb9d4732bbc04ce4363096613fdb8244f673
Rafael Santos <raffrenan@gmail.com>
2022-05-22 16:31:04 -0300

    Add navbar.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,29 +2,31 @@ module.exports = {
   content: [],
   theme: {
     extend: {
-      '14': '3.5rem',
-      '125px': '125px',
+      spacing: {
+        '14': '3.5rem',
+        '125px': '125px',
+      },
+      colors: {
+        'hoverspt': '#18D760',
+        'purple-main': '#2D46B9',
+        'green-main': '#1ED760',
+      },
+      backgroundImage: theme => ({
+        'spotfy-theme': "url('/assets/img/burts.svg')",
+        'spotfy-theme-mobile': "url('/assets/img/burts-mobile.svg')"
+      }),
+      backgroundSize: {
+        '175%': '175%',
+        '195%': '195%',
+      },
+      backgroundPosition: {
+        'banner': '46% 4%',
+        'banner-mobile': 'top 25% center',
+      },
+      fontSize: {
+        '9xl': '9rem',
+      },
     },
-    colors: {
-      'hoverspt': '#18D760',
-      'purple-main': '#2D46B9',
-      'green-main': '#1ED760',
-    },
-    backgroundImage: theme => ({
-      'spotfy-theme': "url('/assets/img/burts.svg')",
-      'spotfy-theme-mobile': "url('/assets/img/burts-mobile.svg')"
-    }),
-    backgroundSize: {
-      '175%': '175%',
-      '195%': '195%',
-    },
-    backgroundPosition: {
-      'banner': '46% 4%',
-      'banner-mobile': 'top 25% center',
-    },
-    fontSize: {
-      '9xl': '9rem',
-    }
   },
   plugins: [],
 }
